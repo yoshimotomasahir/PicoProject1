c001dfd7c0aaf5045cc3f83b3d35738f3f8e9ec2
Masahiro Yoshimoto <yoshimoto@onsanai.com>
2024-07-16 20:38:42 +0900

    RunFastStreamingを追加

---
+++ RunFastStreaming/RunFastStreaming.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <iomanip>
+#include "ps2000.h"
+
+#define space_pad(num) std::setfill(' ') << std::right << std::setw(num)
+
+const double channelInputRanges[] = { 10., 20., 50., 100., 200., 500., 1000., 2000., 5000., 10000., 20000., 50000., 100000., 200000. };
+double adc2mV(int16_t adc, int16_t range, double adcMax = 32767.0) {
+	return double(adc) * channelInputRanges[range] / adcMax;
+}
+
+int16_t mV2adc(double mV, int16_t range, double adcMax = 32767.0) {
+	return std::round(mV * adcMax / channelInputRanges[range]);
+}
+
+double getRange(int16_t range) {
+	return channelInputRanges[range];
+}
+
+#include <windows.h>
+// カーソルを移動
+void setCursorPosition(int x, int y) {
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+
+void getMaxMin(int32_t no_of_values, int16_t bufferA[], int16_t& minValue, int32_t& minIndex, int16_t& maxValue, int32_t& maxIndex) {
+	minValue = bufferA[0];
+	maxValue = bufferA[0];
+	minIndex = 0;
+	maxIndex = 0;
+	for (int i = 1; i < no_of_values; ++i) {
+		if (bufferA[i] < minValue) {
+			minValue = bufferA[i];
+			minIndex = i;
+		}
+		if (bufferA[i] > maxValue) {
+			maxValue = bufferA[i];
+			maxIndex = i;
+		}
+	}
+}
+
+
+void  PREF4 ps2000FastStreamingReady(int16_t** overviewBuffers,
+	int16_t overflow,
+	uint32_t triggeredAt,
+	int16_t triggered,
+	int16_t auto_stop,
+	uint32_t nValues)
+{
+	setCursorPosition(0, 10);
+	std::cout << space_pad(11) << overflow << space_pad(11) << triggeredAt << space_pad(11) << triggered << space_pad(11) << auto_stop << space_pad(11) << nValues;
+	std::cout << std::endl;
+	std::cout << space_pad(11) << overviewBuffers[0][0] << space_pad(11) << overviewBuffers[0][1];
+}
+
+#define BUFFER_SIZE 	1024
+int main() {
+
+	// デバイスをオープン
+	int16_t handle = ps2000_open_unit();
+	if (handle <= 0) {
+		std::cerr << "デバイスのオープンに失敗しました。" << std::endl;
+		return 1;
+	}
+
+	// サンプル数と電圧レンジとサンプリング間隔を設定
+	const int32_t sampleCount = 10000; // 取得するサンプル数
+	int16_t oversample = 1;     // オーバーサンプル (??)
+	int16_t range = PS2000_100MV; // 電圧レンジ設定
+	int16_t timeUnits = PS2000_NS; // 時間単位 (ns)
+	int16_t timeBase = 3;
+	double timeInterval = std::pow(2, timeBase) * 10; // ns
+
+	// チャンネル設定
+	ps2000_set_channel(handle, PS2000_CHANNEL_A, 1, PS2000_DC_VOLTAGE, range);
+	ps2000_set_channel(handle, PS2000_CHANNEL_B, 1, PS2000_DC_VOLTAGE, range);
+
+	// 初期表示
+	std::cout << "Voltage range: (+/-)       [mV]\n";
+	std::cout << "Max voltageAB:             [mV]            [mV] \n";
+	std::cout << "Min voltageAB:             [mV]            [mV] \n";
+	std::cout << "Samples      :              \n";
+	std::cout << "Sample rate  :             [MS/s]\n";
+	std::cout << "Time interval:             [us]\n";
+	std::cout << "Start   time :             [us]\n";
+	std::cout << "End     time :             [us]            [Hz]\n";
+	std::cout << "Elapsed time :             [us]            [Hz]\n";
+	std::cout << std::flush;
+	int x1 = 15;
+	int x2 = 32;
+
+	// トリガー(なし)
+	ps2000_set_trigger(handle, PS2000_NONE, 0, 0, 0, 0);
+
+	int16_t  sample_interval_us = 10;
+	ps2000_run_streaming_ns(handle, sample_interval_us, PS2000_US, sampleCount, 0, 1, 50000);
+
+	int16_t bufferA[sampleCount];
+	int16_t bufferB[sampleCount];
+	int16_t		overflow;
+	int32_t no_of_values;
+	auto start0 = std::chrono::high_resolution_clock::now();
+	auto start = std::chrono::high_resolution_clock::now();
+
+
+	int j = 0;
+	while (true) {
+		ps2000_get_streaming_last_values(handle, ps2000FastStreamingReady);
+		setCursorPosition(0, 9);
+		std::cout << space_pad(11) << j++ ;
+		std::cout << std::flush;
+		std::this_thread::sleep_for(std::chrono::milliseconds(100)); // 10ms 待つ
+	}
+
+	// デバイスを停止
+	ps2000_stop(handle);
+
+	return 0;
+}
